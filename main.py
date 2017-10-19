115efb2ca84e480856e35d1e288897ff26f8a01c
Maxime CORBIN <maxime.corbin@epitech.eu>
2017-10-19 22:31:40 +0200

    Another quote

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ quotes = ["The build failed, your project could not be tested.",
           "# Executing all tests...\n# Test crashed (SIGPIPE)",
           "# Task file not found.",
           "# Got:\n\nReturn value: 84\n# But expected:\nReturn value: 84\n",
-          "mam google"]
+          "mam google",
+          "passed: 0.0 %"]
 
 @client.event
 async def on_message(message):
